0f0f8935b59bc5ce4e7fb2fea62b1500640698d6
maliilma <maliyoujian@gmail.com>
2020-08-31 10:40:30 +0300

    set jre to 14 to avoid warning

--- TodoServiceSpring/src/main/java/com/maliilam/api/todo/TodoApiController.java
+++ TodoServiceSpring/src/main/java/com/maliilam/api/todo/TodoApiController.java
@@ -3,8 +3,6 @@ package com.maliilam.api.todo;
 import java.util.List;
 import java.util.Optional;
 
-import javax.websocket.server.PathParam;
-
 import com.maliilam.api.todo.model.Todo;
 import com.maliilam.api.todo.service.TodoService;
 
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
